21fca3663a4f770080a804aa086d8afd5636e1c7
Junior Ubarana <elci.junior@gmail.com>
2021-03-31 18:05:11 -0300

    feature: static video panel added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,98 @@
-import { Button } from '@material-ui/core';
+import { Box, Grid } from '@material-ui/core';
 
-import Layout from '../components/Layout';
+import Layout from 'src/components/Layout';
+import VideoCard from 'src/components/VideoCard';
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <Layout title="YouTube">
-      Clone YouTube com NextJS & Material-UI
-      <Button variant="outlined" color="secondary">
-        Secondary
-      </Button>
+      <Box p="2">
+        <Grid container spacing={4}>
+          {data.map((item) => (
+            <Grid key={item._id} item xl={3} lg={3} md={4} sm={6} xs={12}>
+              <VideoCard item={item} />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const data = [
+    {
+      id: 1,
+      title: 'NextJS: O framework que você deveria conhecer (parte #01)',
+      authorId: 1,
+      autorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },
+    {
+      id: 2,
+      title:
+        'NextJS: Entendendo a renderização React no lado servidor (parte #02)',
+      authorId: 1,
+      autorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: 'thumbs/next02.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },
+    {
+      id: 3,
+      title: 'NextJS: Totas estáticas e dinâmicas (parte #03)',
+      authorId: 3,
+      autorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },
+    {
+      id: 4,
+      title: 'NextJS: O framework que você deveria conhecer (parte #01)',
+      authorId: 1,
+      autorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next01.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },
+    {
+      id: 5,
+      title:
+        'NextJS: Entendendo a renderização React no lado servidor (parte #02)',
+      authorId: 1,
+      autorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: 'thumbs/next02.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },
+    {
+      id: 6,
+      title: 'NextJS: Totas estáticas e dinâmicas (parte #03)',
+      authorId: 3,
+      autorName: 'Lucas Nhimi',
+      authorAvatar: 'avatarUrl',
+      views: 10,
+      thumb: '/thumbs/next03.png',
+      videoUrl: 'url',
+      updateAt: new Date(),
+    },
+  ];
+  return {
+    props: {
+      data: JSON.parse(JSON.stringify(data)),
+    }, // will be passed to the page component as props
+  };
 }
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,4 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-//import { red } from '@material-ui/core/colors';
 import { colors } from '@material-ui/core';
 
 // Create a theme instance.
